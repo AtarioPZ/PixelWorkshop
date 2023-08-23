593de324fc04f244691cf6c59ca31aeca0907c47
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-08-23 13:15:25 +0530

    UI overlay for incorrect combination

--- src/components/Crafting.js
+++ src/components/Crafting.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import craftingRecipes from './CraftingRecipes';
 
 function Crafting({ selectedItemSlot1, selectedItemSlot2, items, setItems, resetSlots, itemMapping }) {
@@ -7,19 +7,25 @@ function Crafting({ selectedItemSlot1, selectedItemSlot2, items, setItems, reset
     return JSON.stringify(a) === JSON.stringify(b);
   };
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleCrafting = () => {
     if (selectedItemSlot1 && selectedItemSlot2) {
       // Crafting logic based on selected items in the slots
+      let recipeExists = false;
       for (const recipe of craftingRecipes) {
         if (
           (arraysEqual([selectedItemSlot1, selectedItemSlot2], recipe.ingredients.sort()) ||
             arraysEqual([selectedItemSlot2, selectedItemSlot1], recipe.ingredients.sort()))
         ) {
           const newItem = recipe.result;
-          
+
           // Check if the item already exists in the items list
           if (!items.includes(newItem)) {
             setItems((prevItems) => [...prevItems, newItem]);
+          } else {
+            // Recipe already exists
+            recipeExists = true;
           }
 
           // Reset both slots to null
@@ -34,44 +40,73 @@ function Crafting({ selectedItemSlot1, selectedItemSlot2, items, setItems, reset
           return;
         }
       }
+
+      // If recipe does not exist or recipe already exists, show modal
+      if (!recipeExists) {
+        setIsModalOpen(true);
+      }
     }
   };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    // Reset both slots to null when the modal is closed
+    resetSlots();
+    resetSlots();
+  };
+
   return (
-    <div className="nes-container p-3" style={{ backgroundImage: 'url("./assets/background/ybg.jpg")', backgroundSize: '100% 100%' }}>
-      <div className="d-flex flex-column py-5">
-        {/* Display the selected items in the Crafting component */}
-        <div className="d-flex justify-content-center align-items-center py-5">
-          {selectedItemSlot1 ? (
-            <img
-              src={`/assets/sprites/items/${itemMapping[selectedItemSlot1]}`} // Use itemMapping to get the image file name
-              alt={selectedItemSlot1}
-              width={"100px"}
-              height={"100px"}
-            />
-          ) : (
-            <div className="crafting-area-placeholder">Slot 1</div>
-          )}
-          <img
-            src="./assets/sprites/combine.png"
-            alt="combine"
-            width={"50px"}
-            height={"50px"}
-          />
-          {selectedItemSlot2 ? (
-            <img
-              src={`/assets/sprites/items/${itemMapping[selectedItemSlot2]}`} // Use itemMapping to get the image file name
-              alt={selectedItemSlot2}
-              width={"100px"}
-              height={"100px"}
-            />
-          ) : (
-            <div className="crafting-area-placeholder">Slot 2</div>
-          )}
+    <div className="crafting-container">
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <div className="nes-container is-dark with-title is-centered">
+              <p className="title">Crafting Error</p>
+              <p>Combination not found or already exists.</p>
+              <button className="nes-btn" onClick={closeModal}>
+                Close
+              </button>
+            </div>
+          </div>
         </div>
-        {/* Craft button */}
-        <button className="nes-btn mt-2" onClick={handleCrafting}>
-          Craft
-        </button>
+      )}
+
+      <div className="nes-container p-3" style={{ backgroundImage: 'url("./assets/background/ybg.jpg")', backgroundSize: '100% 100%' }}>
+        <div className="d-flex flex-column py-5">
+          {/* Display the selected items in the Crafting component */}
+          <div className="d-flex justify-content-center align-items-center py-5">
+            {selectedItemSlot1 ? (
+              <img
+                src={`/assets/sprites/items/${itemMapping[selectedItemSlot1]}`} // Use itemMapping to get the image file name
+                alt={selectedItemSlot1}
+                width={"100px"}
+                height={"100px"}
+              />
+            ) : (
+              <div className="crafting-area-placeholder">Slot 1</div>
+            )}
+            <img
+              src="./assets/sprites/combine.png"
+              alt="combine"
+              width={"50px"}
+              height={"50px"}
+            />
+            {selectedItemSlot2 ? (
+              <img
+                src={`/assets/sprites/items/${itemMapping[selectedItemSlot2]}`} // Use itemMapping to get the image file name
+                alt={selectedItemSlot2}
+                width={"100px"}
+                height={"100px"}
+              />
+            ) : (
+              <div className="crafting-area-placeholder">Slot 2</div>
+            )}
+          </div>
+          {/* Craft button */}
+          <button className="nes-btn mt-2" onClick={handleCrafting}>
+            Craft
+          </button>
+        </div>
       </div>
     </div>
   );
